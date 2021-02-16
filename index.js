9295e2248ab4c0bb9aab907991dfd4c4cf42557f
Mirzakhid Osmankulov <190103288@stu.sdu.edu.kz>
2021-02-16 11:53:31 +0600

    adding a new file and changing exists files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '1662416532:AAF5QeEx4esalXQgaoKibKtOpKpQsUKDB0k'
-
+const debug = require('./helpers')
 console.log('Bot has been started')
 
 const bot = new TelegramBot(TOKEN, {
@@ -14,5 +14,40 @@ const bot = new TelegramBot(TOKEN, {
 })
 
 bot.on('message', msg => {
-	bot.sendMessage(msg.chat.id, 'I am alive');
+
+const chatId = msg.chat.id
+
+	if (msg.text === 'Close'){
+
+		bot.sendMessage(chatId, 'Close keyboard', {
+			reply_markup: {
+				remove_keyboard: true
+			}
+		})
+
+	} else if (msg.text === 'Reply'){
+
+		bot.sendMessage(chatId, 'Close keyboard', {
+			reply_markup: {
+				force_reply: true
+			}
+		})
+		
+	}else {
+
+
+
+	bot.sendMessage(chatId, 'Klaviatura', {
+		reply_markup: {
+			keyboard: [
+			[{
+				text: 'Send Location',
+				request_location: true
+			}],
+			['Reply','Close'], 
+			['Send Contact']
+			]
+		}
+	})
+}
 })
